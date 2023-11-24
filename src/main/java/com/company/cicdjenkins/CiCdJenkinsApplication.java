7898ed7e56e2913498332f82bc2c9607d8c77c15
doniyorda <davlataliyevdoniyorjon@gmail.com>
2023-11-24 15:03:28 +0500

    add buildyspec.yml

--- src/main/java/com/company/cicdjenkins/CiCdJenkinsApplication.java
+++ src/main/java/com/company/cicdjenkins/CiCdJenkinsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CiCdJenkinsApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(CiCdJenkinsApplication.class, args);
     }
 
